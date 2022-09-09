fe3910c477bee0a8751e6ef245b7fe91d4da790d
MAX <maksimdemidovru@mail.ru>
2022-09-09 10:17:20 +0300

    add Ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Вам потребуется ввести числа, а я выведу количество положительных. Введите количество чисел которые будете использовать: ");
+bool Array = int.TryParse(Console.ReadLine(), out int number);
+
+if (!Array)
+{
+    Console.WriteLine("Не верные данные ввода, попробуйте ввести другие числа");
+}
+
+int GetNumber(int x)
+{
+    int[] array = new int[x];
+    Console.WriteLine("Введите числа: ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    
+    int num = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            num = num + 1;
+        }
+    }
+    return num;
+}
+int positive = GetNumber(number);
+Console.WriteLine($"Количество положительных чисел = {positive}");
+
